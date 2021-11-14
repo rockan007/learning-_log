264ef3fc5a9e4f6c0abcd278014a4a8827e2537b
rockan007 <rockan007@gmail.com>
2021-11-14 08:57:27 +0000

    添加样式

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
+from django.http import Http404
 
 from .models import Topic,Entry
 from .forms import TopicForm,EntryForm
@@ -7,18 +9,25 @@ from .forms import TopicForm,EntryForm
 def index(request):
     return render(request,'learning_logs/index.html')
 
+@login_required
 def topics(request):
-    topics = Topic.objects.order_by('date_added')
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
     context = {'topics':topics}
     return render(request,'learning_logs/topics.html',context)
 
+@login_required
 def topic(request, topic_id):
     """显示单个主题及其所有条目"""
     topic = Topic.objects.get(id=topic_id)
+    # 确认请求的主体属于当前用户
+    if topic.owner != request.user:
+        raise Http404
+    
     entries= topic.entry_set.order_by('date_added')
     context = {'topic':topic,'entries':entries}
     return render(request,'learning_logs/topic.html',context)
 
+@login_required
 def new_topic(request):
     """添加新主题"""
     if request.method!= 'POST':
@@ -28,12 +37,15 @@ def new_topic(request):
         # POST提交的数据，对数据进行处理
         form = TopicForm(data=request.POST)
         if form.is_valid():
-            form.save()
+            new_topic=form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             return redirect('learning_logs:topics')
     # 显示空表单或指出表单数据无效
     context ={'form':form}
     return render(request,'learning_logs/new_topic.html',context)
 
+@login_required
 def new_entry(request,topic_id):
     """在特定主题中添加新条目"""
     topic = Topic.objects.get(id= topic_id)
@@ -51,9 +63,13 @@ def new_entry(request,topic_id):
     context = {'topic':topic, 'form': form}
     return render(request,'learning_logs/new_entry.html',context)
 
+@login_required
 def edit_entry(request,entry_id):
     entry = Entry.objects.get(id= entry_id)
     topic = entry.topic
+
+    if topic.owner != request.user:
+        raise Http404
 
     if request.method != 'POST':
         form = EntryForm(instance=entry)
